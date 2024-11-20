1c55e318edf048cd42eeda5be8213e2a624faadf
Gnanendhar Reddy <gnanendharreddy8@gmail.com>
2024-11-21 00:48:01 +0530

    adds CRUD functionalities for Product

---
+++ Task/src/main/java/com/esd/task/Entity/ProductEntity.java
@@ -0,0 +1,41 @@
+package com.esd.task.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ Task/src/test/java/com/esd/task/TaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.esd.task;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
